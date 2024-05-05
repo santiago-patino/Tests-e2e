286d25ecf0562736b86c01ee6ae2b28311625df0
ekcondev <e.condev@uniandes.edu.co>
2024-05-04 22:18:37 -0500

    Escenario Crear miembro con datos inválidos

--- kraken/features/1new_member_invalid_data.feature
+++ kraken/features/1new_member_invalid_data.feature
@@ -14,8 +14,13 @@ Scenario: Como admin intento crear un nuevo member con un correo no válido y ve
         And I wait for 2 seconds
     Then Debería tener un form para ingresar la información del member
 
+    When Dar click en Save
+    Then Debería tener un error de datos vacíos 
+        And I wait for 3 seconds
+
     When Ingresar el nombre del member
         And Ingresar el email inválido del member
         And Dar click en Save
     Then Debería tener un error de datos inválidos
         And I wait for 3 seconds
+        And Volver a la sección miembros
--- kraken/features/web/step_definitions/new_member_invalid_data.js
+++ kraken/features/web/step_definitions/new_member_invalid_data.js
@@ -7,14 +7,33 @@ When("Ingresar el email inválido del member", async function () {
   return await element.setValue(randomEmail);
 });
 
-Then(
-    "Debería tener un error de datos inválidos",
-    async function () {
-      let memberNameElement = await this.driver.$(
-        "div.gh-list-scrolling table.gh-list tbody tr a.ember-view h3.gh-members-list-name "
-      );
-      let memberName = await memberNameElement.getText();
-      expect(memberName).to.include("Edna Conde");
-      await memberNameElement.click();
-    }
+Then("Debería tener un error de datos vacíos", async function () {
+  let responseElement = await this.driver.$(
+    "form.member-basic-info-form div.form-group.max-width.error.ember-view p.response"
   );
+  let responseText = await responseElement.getText();
+  expect(responseText).to.include("Please enter an email.");
+});
+
+Then("Debería tener un error de datos inválidos", async function () {
+  let responseElement = await this.driver.$(
+    "form.member-basic-info-form div.form-group.max-width.error.ember-view p.response"
+  );
+  let responseText = await responseElement.getText();
+  expect(responseText).to.include("Invalid Email.");
+});
+
+When("Volver a la sección miembros", async function () {
+  let element = await this.driver.$('a[href="#/members/"]');
+  await element.click();
+  await this.driver.waitUntil(async () => {
+    let confirmButton = await this.driver.$(
+      "div.fullscreen-modal.fullscreen-modal-action.fullscreen-modal-wide section.modal-content.ember-view div.modal-footer button.gh-btn.gh-btn-red"
+    );
+    return confirmButton && (await confirmButton.isClickable());
+  });
+  let confirmButton = await this.driver.$(
+    "div.fullscreen-modal.fullscreen-modal-action.fullscreen-modal-wide section.modal-content.ember-view div.modal-footer button.gh-btn.gh-btn-red"
+  );
+  await confirmButton.click();
+});
